243f1076730d96f93527cf68f09721437a3d60a1
jagritilakra <47425334+jagritilakra@users.noreply.github.com>
2019-02-07 23:45:46 +0530

    Create Mainclass.java

---
+++ Mainclass.java
@@ -0,0 +1,13 @@
+package google_tests;
+import google_tests.Testcases;
+public class Mainclass extends Testcases{
+
+	public static void main(String[] args) {
+		Testcases obj3=new Testcases();
+		obj3.Gettexts();
+		obj3.linkcheck();
+		
+
+	}
+
+}
