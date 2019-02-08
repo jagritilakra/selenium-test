cb61b42b8a1cac3be0de1815a22d00a01337abbe
jagritilakra <47425334+jagritilakra@users.noreply.github.com>
2019-02-08 09:43:10 +0530

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -1,7 +1,7 @@
 package google_tests;
 import google_tests.Testcases;
 public class Mainclass extends Testcases{
-
+//this class executes methods for Testcases.java
 	public static void main(String[] args) {
 		Testcases obj3=new Testcases();
 		obj3.Gettexts();
